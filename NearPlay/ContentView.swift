922b7f1f91f7ecfe603c6d54ad76793649869c0a
Andrei Musca <andrei.m150@yahoo.com>
2026-06-30 14:50:13 +0300

    Initial Commit

---
+++ NearPlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NearPlay
+//
+//  Created by Andrei Musca on 30/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NearPlay/NearPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearPlayApp.swift
+//  NearPlay
+//
+//  Created by Andrei Musca on 30/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct NearPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
